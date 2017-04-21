3848cb8d7897ac2adc8a5f04308b79170bec9040
Ricardo Bomfim <ricardoub@gmail.com>
2017-04-21 14:02:00 -0300

    LAYOUT- CHANGES for panel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,3 @@
-@extends('layouts.panel')
-
 @section('content')
 <div class="container">
     <div class="row">
@@ -15,3 +13,17 @@
     </div>
 </div>
 @endsection
+
+@extends('layouts.panel')
+
+@section('panel-head-middle')
+  Dashboard
+@endsection
+
+@section('panel-body')
+    You are logged in!
+@endsection
+
+@section('scripts')
+   <!--<script src="/js/todos.js"></script>-->
+@stop
