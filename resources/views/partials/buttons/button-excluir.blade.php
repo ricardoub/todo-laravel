095785bd1ee05b5a456a7135df262e5b027eca88
Ricardo Bomfim <ricardoub@gmail.com>
2017-04-22 11:00:48 -0300

    PARTIALS - buttons included

---
+++ resources/views/partials/buttons/button-excluir.blade.php
@@ -0,0 +1,5 @@
+<button type="button" class="btn btn-default"
+  data-toggle="modal" data-target="#modalExcluir">
+  <i class="fa fa-trash fa-fw"></i>
+  <span class="hidden-xs hidden-sm"> Excluir </span>
+</button>
