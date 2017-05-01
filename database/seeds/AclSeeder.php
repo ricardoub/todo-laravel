bff37ed0f16212bf8e041dd6f67d85a2055f3e4a
Ricardo Bomfim <ricardoub@gmail.com>
2017-05-01 12:22:41 -0300

    acl: seeder chaged

--- database/seeds/AclSeeder.php
+++ database/seeds/AclSeeder.php
@@ -8,51 +8,98 @@ use App\User;
 
 class AclSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    /*
+     * ROLES
      */
-    public function run()
-    {
-        $adminRole = new Role();
-        $adminRole->name         = 'admin';
-        $adminRole->display_name = 'User Administrator'; // optional
-        $adminRole->description  = 'User is allowed to manage and edit other users'; // optional
-        $adminRole->save();
 
-        $ownerRole = new Role();
-        $ownerRole->name         = 'owner';
-        $ownerRole->display_name = 'Todo owner'; // optional
-        $ownerRole->description  = 'User is the owner of a given todo'; // optional
-        $ownerRole->save();
+    $todoRole         = Role::where('name', '=', 'todo')->first();
+    $todoCommonRole   = Role::where('name', '=', 'todo-common')->first();
+    $todoAdvancedRole = Role::where('name', '=', 'todo-advanced')->first();
 
-        $createTodo = new Permission();
-        $createTodo->name         = 'create-todo';
-        $createTodo->display_name = 'Create Todo'; // optional
-        // Allow a user to...
-        $createTodo->description  = 'create new todo posts'; // optional
-        $createTodo->save();
+    $registerRole         = Role::where('name', '=', 'register')->first();
+    $registerCommonRole   = Role::where('name', '=', 'register-common')->first();
+    $registerAdvancedRole = Role::where('name', '=', 'register-advanced')->first();
 
-        $editUser = new Permission();
-        $editUser->name         = 'edit-user';
-        $editUser->display_name = 'Edit Users'; // optional
-        // Allow a user to...
-        $editUser->description  = 'edit existing users'; // optional
-        $editUser->save();
+    $adminRole = Role::where('name', '=', 'admin')->first();
+    /*
+     * PERMISSIONS
+     */
 
-        $adminRole->attachPermission($createTodo, $editUser);
-        // equivalent to $adminRole->perms()->sync(array($createPost->id));
+    // todoRole
+    $todoIndex   = Permission::where('name', '=', 'todo-index')->first();
+    $todoShow    = Permission::where('name', '=', 'todo-show')->first();
+    $todoCreate  = Permission::where('name', '=', 'todo-create')->first();
+    $todoEdit    = Permission::where('name', '=', 'todo-edit')->first();
+    $todoDelete  = Permission::where('name', '=', 'todo-delete')->first();
+    $todoOwner   = Permission::where('name', '=', 'todo-owner')->first();
+    $todoAdmin   = Permission::where('name', '=', 'todo-admin')->first();
 
-        $ownerRole->attachPermissions(array($createTodo));
-        // equivalent to $ownerRole->perms()->sync(array($createPost->id, $editUser->id));
+    // userRole
+    $userIndex   = Permission::where('name', '=', 'user-index')->first();
+    $userShow    = Permission::where('name', '=', 'user-show')->first();
+    $userAdmin   = Permission::where('name', '=', 'user-admin')->first();
 
-        $user = User::where('name', '=', 'Administrador')->first();
+    // comboRole
+    $comboIndex  = Permission::where('name', '=', 'combo-index')->first();
+    $comboShow   = Permission::where('name', '=', 'combo-show')->first();
+    $comboCreate = Permission::where('name', '=', 'combo-create')->first();
+    $comboEdit   = Permission::where('name', '=', 'combo-edit')->first();
+    $comboDelete = Permission::where('name', '=', 'combo-delete')->first();
 
-        // role attach alias
-        //$user->attachRole($adminRole); // parameter can be an Role object, array, or id
+    /*
+     * ACL
+     */
 
-        // or eloquent's original technique
-        $user->roles()->attach($adminRole->id); // id only
-    }
+    $todoRole->attachPermissions(
+      array($todoIndex, $todoShow));
+    $todoCommonRole->attachPermissions(
+      array($todoCreate, $todoEdit));
+    $todoAdvancedRole->attachPermissions(
+      array($todoDelete, $todoOwner));
+
+    $registerRole->attachPermissions(
+      array(
+        $comboIndex, $comboShow,
+        $userIndex, $userShow));
+    $registerCommonRole->attachPermissions(
+      array(
+        $comboCreate, $comboEdit));
+    $registerAdvancedRole->attachPermissions(
+      array(
+        $comboDelete));
+
+    $adminRole->attachPermissions(
+      array(
+        $todoAdmin, $userAdmin));
+
+    /*
+     * USERS 
+     */
+
+    $userCommon = User::where('name', '=', 'Usuario1')->first();
+    $userCommon->roles()->attach($todoRole->id);
+    $userCommon->roles()->attach($todoCommonRole->id);
+
+    $userAdvanced = User::where('name', '=', 'Usuario2')->first();
+    $userAdvanced->roles()->attach($todoRole->id);
+    $userAdvanced->roles()->attach($todoCommonRole->id);
+    $userAdvanced->roles()->attach($todoAdvancedRole->id);
+    $userAdvanced->roles()->attach($registerRole->id);
+
+    $userAdmin = User::where('name', '=', 'Administrador')->first();
+    $userAdmin->roles()->attach($todoRole->id);
+    $userAdmin->roles()->attach($todoCommonRole->id);
+    $userAdmin->roles()->attach($todoAdvancedRole->id);
+    $userAdmin->roles()->attach($registerRole->id);
+    $userAdmin->roles()->attach($registerCommonRole->id);
+    $userAdmin->roles()->attach($registerAdvancedRole->id);
+    $userAdmin->roles()->attach($adminRole->id);
+  }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,15 @@ class DatabaseSeeder extends Seeder
   public function run()
   {
     $this->call(UsersTableSeeder::class);
+
+    $this->call(RolesTableSeeder::class);
+    $this->call(PermissionsTableSeederUsers::class);
+    $this->call(PermissionsTableSeederTodos::class);
+    $this->call(PermissionsTableSeederCombos::class);
     $this->call(AclSeeder::class);
-    $this->call(TodosTableSeeder::class);
+
     $this->call(CombosTableSeeder::class);
+
+    $this->call(TodosTableSeeder::class);
   }
 }
